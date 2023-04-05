a6f812d4eeb49337326267fb9a17bcea05cd6fb9
lei <sanyue0315@outlook.com>
2023-04-05 12:08:47 +0800

    optimized

--- pamac/proxy.sh
+++ pamac/proxy.sh
@@ -2,7 +2,7 @@
 # 该脚本用于处理pamac安装软件时，由github下载缓慢甚至无法下载的问题
 # 检测域名是不是github，如果是，则使用代理进行下载
 domain=`echo $2 | cut -f3 -d'/'`
-if [[ $domain=~"github.com" ]];then
+if [[ $domain =~ "github.com" ]];then
     export https_proxy="socks5://localhost:7891"
     echo "download from github $2 using proxy"
 fi
